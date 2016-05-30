fea348132e1bc85cfc0b496ceaeced75e782be2d
Jennifer Bennett <jennbbennett@hotmail.com>
2016-05-30 09:03:38 -0600

    writing tests

--- spec/spec.js
+++ spec/spec.js
@@ -1,13 +1,27 @@
 var myCode = require('../src');
 
-describe('spiralMatrix', function() {
-  it('returns the input integer', function() {
-    expect(myCode.spiralMatrix(14)).toEqual(14);
-  });
+describe('dimensionCalc', function () {
+    it('returns the dimension of the matrix based on num', function () {
+        expect(myCode.dimensionCalc(0)).toEqual(1);
+        expect(myCode.dimensionCalc(11)).toEqual(4);
+        expect(myCode.dimensionCalc(24)).toEqual(5);
+    });
 });
 
-// describe('matches', function() {
-//   it('returns true if the given arguments match', function() {
-//     expect(yourCode.matches('a', 'a')).toEqual(true);
-//   });
-// });
+describe('initializeGrid', function () {
+    it('returns the two dimensional array needed to form a spiral matrix of num', function () {
+        expect(myCode.initializeGrid(5)).toEqual([['-','-','-','-','-'],['-','-','-','-','-'],['-','-','-','-','-'],['-','-','-','-','-'],['-','-','-','-','-']]);
+        expect(myCode.initializeGrid(3)).toEqual([['-','-','-'],['-','-','-'],['-','-','-']]);
+    });
+});
+
+
+describe('populateGrid', function () {
+    xit('returns a spiral matrix with zero at the center', function () {
+        expect(myCode.populateGrid(15)).toEqual();
+    });
+    xit('returns a matrix containing values from 0 to num', function () {
+        expect(myCode.populateGrid(10)).toEqual();
+    });
+});
+
--- src.js
+++ src.js
@@ -1,10 +1,81 @@
 module.exports = {
 
-spiralMatrix:  function(num){
-  return num;
-}
+    dimensionCalc: function (num) {
+        return Math.ceil(Math.sqrt(num + 1));
+    },
+    initializeGrid: function (size) {
+        var grid = [];
+        for (var i = 0; i < size; i++) {
+            var innerArray = [];
+            for (var j = 0; j < size; j++) {
+                innerArray[j] = '-';
+            }
+            grid[i] = innerArray;
+        }
+        return grid;
+    },
+    populateGrid: function (num) {
+        var size = dimensionCalc(num);
+        var grid = initializeGrid(size);
+        var x = Math.floor((size - 1) / 2);
+        var y = x;
+        var xMin = x;
+        var yMin = y;
+        var xMax = x + 1;
+        var yMax = y;
+        var direction = "right";
+        for (var counter = 0; counter <= num; counter++) {
+            grid[y][x] = counter;
+            //printGrid(grid);
+            switch (direction) {
+                case "right":
+                    if (x < xMax) {
+                        x++
+                    } else {
+                        direction = "down";
+                        yMax++;
+                        y++
+                    }
+                    break;
 
+                case "down":
+                    if (y < yMax) {
+                        y++;
+                    } else {
+                        direction = "left";
+                        xMin--;
+                        x--;
+                    }
+                    break;
+                case "left":
+                    if (x > xMin) {
+                        x--;
+                    } else {
+                        direction = "up";
+                        yMin--;
+                        y--;
+                    }
+                    break;
+                case "up":
+                    if (y > yMin) {
+                        y--;
+                    } else {
+                        direction = "right";
+                        xMax++;
+                        x++;
+                    }
+                    break;
+            }
+        }
+        return grid;
+    },
 
-
-
+    printGrid: function (grid) {
+        console.log('----------------------');
+        for (var i = 0; i < grid.length; i++) {
+            var row = grid[i].join('\t');
+            console.log(row);
+        }
+        console.log('----------------------');
+    }
 };
